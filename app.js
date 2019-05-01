99953c84f66ee5f31375e85f7ea66aed5bf48444
Sergio Antonio Gutiérrez Zebadua <sergio.gutierrez.zebadua@gmail.com>
2019-05-01 14:44:09 -0500

    get usuario

--- app.js
+++ app.js
@@ -6,32 +6,27 @@ var mongoose = require('mongoose');
 // Inicializar variables
 var app = express();
 
+// Importar rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+
 //Conecxión a la base de datos
-// mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (Err, res) => {
-
-//     if (err) throw err;
-
-//     console.log('Base de datos: \x1b[32m%s\x1b[0m', 'online');
-// })
 
 mongoose.connect('mongodb://localhost:27017/hospitalDB', { useNewUrlParser: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
     // Conectado
-    console.log("Base de datos: \x1b[32m%s\x1b[0m", " online")
+    console.log("Base de datos: \x1b[32m%s\x1b[0m", " online");
 });
-// Rutas
-app.get('/', (req, res, next) => {
 
-    res.status(200).json({
-        ok: true,
-        mensaje: 'Peticion realizada correctamente'
-    });
-});
+// Rutas
+app.use('/usuario', usuarioRoutes);
+app.use('/', appRoutes);
+
 
 // Escuchar peticiones
 app.listen(3000, () => {
 
     console.log('Express server puerto 3000: \x1b[32m%s\x1b[0m', 'online');
-})
+});
