59b179ed15c80cececd934eccd4285ce0b938445
Sergio Antonio Gutiérrez Zebadua <sergio.gutierrez.zebadua@gmail.com>
2019-05-04 18:45:51 -0500

    Usuario y token listo

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
 //Requires
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 
 // Inicializar variables
 var app = express();
 
+//=============================================================================
+//=============================================================================
+// Body Parser
+//=============================================================================
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+
+//=============================================================================
+// Body Parser
+//=============================================================================
+//=============================================================================
 // Importar rutas
 var appRoutes = require('./routes/app');
 var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
 
 //Conecxión a la base de datos
 
@@ -22,6 +39,7 @@ db.once('open', function() {
 
 // Rutas
 app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
 app.use('/', appRoutes);
 
 
